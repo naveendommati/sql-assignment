11a92e810da172a75d96863a5679c32b19059a54
naveendommati <118064363+naveendommati@users.noreply.github.com>
2023-01-29 22:06:21 +0530

    Create assignment1.sql

---
+++ assignment1.sql
@@ -0,0 +1,13 @@
+ create table city
+    (ID int, NAME VARCHAR(17),COUNTRYCODE varchar(3),
+    DISTRICT varchar(20),
+    POPULATION int);
+   select * from city;
+  1) answer-- select * from city where COUNTRYCODE ='USA' and POPULATION >100000;
+  2) answer-- select NAME from city where COuNTRYCODE ='USA' AND POPULATION >120000;
+  3) answer-- select * from city;
+  4)answer--  select * from city where ID =1661;
+  5)answer -- select * from city where COUNTRYCODE= 'JPN';
+  6)answer-- select NAME from city where COUNTRYCODE ='JPN';
+  7)answer--
+  
