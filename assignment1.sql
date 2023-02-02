cb1a9ef38aca874cf72576ea60d6c2a759fc6e2d
naveendommati <118064363+naveendommati@users.noreply.github.com>
2023-02-02 20:05:45 +0530

    Update assignment1.sql

--- assignment1.sql
+++ assignment1.sql
@@ -64,3 +64,45 @@ having sum(unit) >= 100;
 27) answer-- SELECT *
 FROM Users
 WHERE REGEXP_LIKE(mail, '^[a-zA-Z][a-zA-Z0-9\_\.\-]*@leetcode.com');
+
+28) answer-- SELECT a.customer_id,
+       a.name
+FROM Customers AS a
+INNER JOIN
+  (SELECT *
+   FROM Orders
+   WHERE order_date BETWEEN "2020-06-01" AND "2020-07-31" ) AS b
+ON a.customer_id = b.customer_id
+INNER JOIN Product AS c
+ON b.product_id = c.product_id
+GROUP BY a.customer_id
+HAVING SUM(CASE
+               WHEN LEFT(b.order_date, 7) = "2020-06" THEN c.price * b.quantity
+               ELSE 0
+           END) >= 100
+AND    SUM(CASE
+               WHEN LEFT(b.order_date, 7) = "2020-07" THEN c.price * b.quantity
+               ELSE 0
+           END) >= 100
+ORDER BY NULL;
+29) answer--- select distinct title
+from Content 
+join TVProgram using(content_id)
+where kids_content = 'Y' 
+	and content_type = 'Movies' 
+	and (month(program_date), year(program_date)) = (6, 2020);
+	
+30) answer--
+select NPV.* from NPV left join
+Queries on NPV.id = Queries.id and NPV.year = Queries.year;
+
+31) answer-- select NPV.* from NPV left join
+Queries on NPV.id = Queries.id and NPV.year = Queries.year;
+
+32) answer-- select unique_id, name
+from Employees
+left join EmployeeUNI
+on if (Employees.id = EmployeeUNI.id, Employees.id, null);
+
+33) answer-- 
+	
